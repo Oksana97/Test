0bdf690b0d4e94201363e768400a62935ffdad75
Oksana97 <oksanakharechko97@ukr.net>
2022-01-11 18:45:12 +0200

    init

--- task1.js
+++ task1.js
@@ -1,1 +1,3 @@
 const arr = ["Adam", "Alex","Monte", "Aaron", "Ben", "Carl", "Dan",  "Tim","David", "Edward", "Fred", "Frank", "George", "Hal", "Ike", "John", "Alex","Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto",  "Peter", "Roger",  "Steve",  "Ben","Thomas", "Tim", "Ty", "Victor", "Walter"];
+
+
