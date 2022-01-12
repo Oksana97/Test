11da4a782d62c486cbdaad7f1744b92f09adbd86
Oksana97 <oksanakharechko97@ukr.net>
2022-01-12 16:45:58 +0200

    init

--- index4.2.js
+++ index4.2.js
@@ -1,5 +1,28 @@
+let arrayNumbers = [
+  [3, 7, 12, 13, , 0 ,17, 21, ];
+  
+];
 
-    newFunction();
+    function selectMainDiagonal(items) {
+    let length = items.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                if (i == j){
+                 arrayNumbers.push(arr[i][j]);
+            }
+        }
+        }
+    }
+    
+    selectMainDiagonal(arr);
+    console.log(arrayNumbers);
+    let arr = []
+
+
+  
+  
+  
+  newFunction();
 
 
 function newFunction() {
